b11cbd08525d49ddadbc779c2b64280a9635da1e
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-16 14:59:42 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 // Function to handle the search criteria via button click
 function handleClickSearch() {
 
-    const query = d3.select('#search_query').property("value");
-
-    if (query) {
-        console.log(query);
-        document.getElementById("search_query").placeholder=query;
-
+    
         var url = "https://hello-world-l.herokuapp.com/";
         var updated_url = url + "hamburguesas-good-mark-de-carne-4-u-320-gr";
 
@@ -14,20 +9,6 @@ function handleClickSearch() {
           .then(function (response) {
             return response.json();
           })
-          .then(function (data) {
-
-                recipeData = data;
-                buildTable(recipeData);
-                var num_query_results = recipeData.length;
-
-                document.getElementById("search_num").innerHTML = "Your query returned " + num_query_results + " results.";
-          })
-
-          .catch(function (err) {
-            console.log(err);
-          });
-    }
-}
-
+          
 // Attach an event to listen for the search recipes button
 d3. select("#search-btn").on("click", handleClickSearch);
