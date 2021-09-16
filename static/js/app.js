96149ce407e530f6669edb1c16c7186abd49c283
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-16 18:27:48 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,11 @@
 // Function to handle the search criteria via button click
 function handleClickSearch() {
 
+    var url = "https://hello-world-l.herokuapp.com/";
+    var updated_url = url + "hamburguesas-good-mark-de-carne-4-u-320-gr";
+    
     document.getElementById("search_num").innerHTML = "Holas!";
+    document.getElementById("file").innerHTML = updated_url;
     
 }
 
