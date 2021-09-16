5c249bb735b52fc8308f9e37e816bd6fd697cdc9
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-16 18:09:35 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -17,5 +17,5 @@ function handleClickSearch() {
 }
 
 // Attach an event to listen for the search recipes button
-// d3.select("#search-btn").on("click", handleClickSearch);
+d3.select("#search-btn").on("click", handleClickSearch);
 document.select("#search-btn").on("click", handleClickSearch);
