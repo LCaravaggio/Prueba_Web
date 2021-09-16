778e2616819a4f65ea8021acccedfb282b7cb2c6
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-16 15:09:22 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,10 +5,32 @@ function handleClickSearch() {
         var url = "https://hello-world-l.herokuapp.com/";
         var updated_url = url + "hamburguesas-good-mark-de-carne-4-u-320-gr";
 
+
         fetch(updated_url)
           .then(function (response) {
             return response.json();
-          })
-          
+          }
+
+        // Start file download.
+        download("hello.txt","This is the content of my file :)");
+        
+}
+
 // Attach an event to listen for the search recipes button
 d3. select("#search-btn").on("click", handleClickSearch);
+
+    
+    
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
