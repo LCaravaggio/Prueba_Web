1b080d06ddc7600ccbb8979ec59a9f7424fd2475
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-16 22:44:48 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -74,6 +74,15 @@ for (var i = 0; i < links.length; i++) {
             console.log(err);
           });    
     }
+
     
-download("hello.txt",al_archivo);    
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+
+download(year + "-" + month + "-" + date + "-" + hours + "-" + minutes + "-" + seconds+".csv",al_archivo);    
 }
