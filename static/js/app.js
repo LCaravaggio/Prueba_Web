aa69ea572c25ceb4c44ab3613e555b933b5fe5f2
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2021-09-23 18:50:45 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,10 @@
+var al_archivo="";
+var TXT="";
+
+
 function handleClickSearch() {
    //var links=TXT.split("\n");
-   document.getElementById("file").innerHTML="Bajando";   
+   document.getElementById("file").innerHTML="Bajando 2";   
 }
 
 d3.select("#search-btn").on("click", handleClickSearch);
